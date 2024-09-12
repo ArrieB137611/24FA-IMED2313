78c174082ae02a398e2d49dd8ce89d51d70df3bb
ArrieB137611 <lilkittencat2014@gmail.com>
2024-09-12 10:23:10 -0500

    add website files and figma

---
+++ Scripts/variables.js
@@ -0,0 +1,16 @@
+//define varibles
+var first = "Arrielle";
+var last = "Brooks";
+var sentence = "Space is wonderful."
+var ten = "10"
+var fifteen = "15"
+var twentyFive = "25"
+var theTruth = true;
+//print out first and last name
+console.log(first + " " + last);
+//print out length of sentence
+console.log("The length of my sentence is: "+ sentence.length + " characters long.");
+//print out some math
+console.log("The sum of "+ ten + " and "+ fifteen + " is equal to "+ twentyFive + ".")
+//print out the data type
+console.log("The variable theTruth is storing the value "+ theTruth + ". The data type of theTruth is " + typeof theTruth + "." )
